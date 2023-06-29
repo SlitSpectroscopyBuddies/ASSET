6269c34d6b439550fae442e9b11e130dc517e7e8
Samuel Thé <the.samuel.ad@gmail.com>
2023-06-29 23:20:44 +0200

    add dependencies for julia package

--- src/ASSET.jl
+++ src/ASSET.jl
@@ -1,6 +1,23 @@
 
 module ASSET
 
+export 
+    AbstractBkg,
+    BkgMdl,
+    bkg_step!,
+    extract_spectrum!,
+    fit_psf_center!,
+    fit_psf_params!,
+    fit_spectrum!,
+    loss,
+    mask_object,
+    object_step!,
+    ParametrizedBkgMdl,
+    psf_map!,
+    solve_analytic!,
+    solve_vmlmb!
+
+import Base: +
 using InterpolationKernels
 using InverseProblem
 using LazyAlgebra
