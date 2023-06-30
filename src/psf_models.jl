1f3ad167d3faa751d890a959898d95dae8372259
Laurence <ldenneulin@montesquiou.info>
2023-06-30 10:26:13 +0200

    "Imported parameters() function of PointSpreadFunctions.jl for our PSF models. Added Moffat model"

--- src/psf_models.jl
+++ src/psf_models.jl
@@ -12,13 +12,14 @@
 #
 # This file is part of ASSET
 
-
+import PointSpreadFunctions:
+    parameters
 
 """
-        h=chromPSF(α)
+        h=chromGaussianPSF(a)
     
 gives an AbstractPSF, more precisely a gaussian chromatic 
-PSF parametred by α, such that the gaussian variance is  σ² = αλ².
+PSF parametred by a, such that the gaussian variance is  σ² = aλ².
 
 Then: 
 
@@ -34,31 +35,30 @@ which is:
 
         exp(-1/2 ρ²/σ²)/sqrt(2πσ²)
     
-""" chromPSF
+""" chromGaussianPSF
 
-struct chromPSF <: AbstractPSF{1}
-    α::Float64
-    chromPSF(α::Float64) = new(α)
+struct chromGaussianPSF <: AbstractPSF{1}
+    a::Float64
 end
  
-function (P::chromPSF)(ρ::T,λ::T) where {T<:AbstractFloat}
-    σ² = P.α*λ*λ
+function (P::chromGaussianPSF)(ρ::T,λ::T) where {T<:AbstractFloat}
+    σ² = P.a*λ*λ
     return exp(-1/2 *ρ*ρ/σ²)/sqrt(2*pi*σ²)
 end
 
-
+@inline parameters(P::chromGaussianPSF) = getfield(P, :a)
 
 """
-        h=chromwmwPSF(p)
+        h=chromwmwGaussianPSF(p)
     
 gives an AbstractPSF, more precisely a gaussian chromatic 
 PSF parametred by two variables given in a vector:
 
- - α the chromatic scalling
+ - a the chromatic scalling
    
- - β the minimum width
+ - b the minimum width
    
-such that the the variance of the PSF is  σ² = αλ² + β
+such that the the variance of the PSF is  σ² = aλ² + b
 
 Then: 
 
@@ -74,18 +74,110 @@ which is:
 
         exp(-1/2 ρ²/σ²)/sqrt(2πσ²)
 
-""" chromwmwPSF
+""" chromwmwGaussianPSF
 
-struct chromwmwPSF <: AbstractPSF{1}
-    α::Float64
-    β::Float64
-    chromwmwPSF(p::AbstractArray{Float64,1}) = new(p[1], p[2])
+struct chromwmwGaussianPSF <: AbstractPSF{1}
+    a::Float64
+    b::Float64
+    chromwmwGaussianPSF(p::AbstractArray{Float64,1}) = new(p[1], p[2])
 end
     
-function (P::chromwmwPSF)(ρ::T,λ::T) where {T<:AbstractFloat}
-    σ² = P.α*λ*λ + P.β
+function (P::chromwmwGaussianPSF)(ρ::T,λ::T) where {T<:AbstractFloat}
+    σ² = P.a*λ*λ + P.b
     return exp(-1/2 *ρ*ρ/σ²)/sqrt(2*pi*σ²)
 end
 
+@inline parameters(P::chromwmwGaussianPSF) = (getfield(P, :a),getfield(P, :b))
+
+
+"""
+        h=chromMoffatPSF(p)
+    
+gives an AbstractPSF, more precisely a centrosymetric Moffat chromatic 
+PSF parametred by two variables given in a vector:
+
+ - a the chromatic scalling
+   
+ - β the Moffat parameter
+   
+such that the the variance of the PSF is  σ² = aλ²
+
+Then: 
+
+        h(ρ,λ) 
+    
+gives the value of the centrosymetric Moffat chromatic PSF for:
+
+ - ρ, a float giving the position in the slit (in pixels)
+   
+ - λ, a float giving the wavelength (in μm)
+    
+which is:
+
+        (1 + ρ²/σ²)^(-β)
+
+""" chromMoffatPSF
+
+struct chromMoffatPSF <: AbstractPSF{1}
+    a::Float64
+    β::Float64
+    chromMoffatPSF(p::AbstractArray{Float64,1}) = new(p[1], p[2])
+end
+
+ 
+function (P::chromMoffatPSF)(ρ::T,λ::T) where {T<:AbstractFloat}
+    σ² = P.a*λ*λ
+    return (1 + ρ*ρ/σ²)^(-P.β)
+end
+
+@inline parameters(P::chromMoffatPSF) = (getfield(P, :a),getfield(P, :β))
+
+
+"""
+        h=chromwmwMoffatPSF(p)
+    
+gives an AbstractPSF, more precisely a centrosymetric Moffat chromatic 
+PSF parametred by two variables given in a vector:
+
+ - a the chromatic scalling
+   
+ - b the minimum width
+ 
+ - β the Moffat parameter
+   
+such that the the variance of the PSF is  σ² = aλ² + b
+
+Then: 
+
+        h(ρ,λ) 
+    
+gives the value of the centrosymetric Moffat chromatic PSF for:
+
+ - ρ, a float giving the position in the slit (in pixels)
+   
+ - λ, a float giving the wavelength (in μm)
+    
+which is:
+
+        (1 + ρ²/σ²)^(-β)
+
+""" chromwmwGaussianPSF
+
+struct chromwmwMoffatPSF <: AbstractPSF{1}
+    a::Float64
+    b::Float64
+    β::Float64
+    chromwmwMoffatPSF(p::AbstractArray{Float64,1}) = new(p[1], p[2], p[3])
+end
+
+ 
+function (P::chromwmwMoffatPSF)(ρ::T,λ::T) where {T<:AbstractFloat}
+    σ² = P.a*λ*λ +P.b
+    return (1 + ρ*ρ/σ²)^(-P.β)
+end
+
+@inline parameters(P::chromwmwMoffatPSF) = (getfield(P, :a),
+                                            getfield(P, :b),
+                                            getfield(P, :β))
 
 
