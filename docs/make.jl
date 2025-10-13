ea596841751941da4dc8ff894e9d817d85d6b994
Laurence DENNEULIN <laurence.denneulin@epita.fr>
2025-10-13 17:31:45 +0200

    updated make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,3 +2,7 @@ using Documenter, ASSET
 
 makedocs(sitename="ASSET documentation", 
          repo=Remotes.GitHub("SlitSpectroscopyBuddies", "ASSET"))
+         
+deploydocs(
+    repo = "github.com/SlitSpectroscopyBuddies/ASSET.git"),
+)
